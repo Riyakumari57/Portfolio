b7f9a8b007d2ea19b067e9bb99e251f81594e33c
Riya Kumari <65845230+Riyakumari57@users.noreply.github.com>
2021-05-04 12:04:37 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,51 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if (this.scrollY > 20) {
+            $('.navbar').addClass("sticky");
+        }
+        else {
+            $('.navbar').removeClass("sticky");
+        }
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+       //toggle menu/navbar script
+    $('.menu-btn').click(function()
+    {
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    // owl carousel script
+ $('.carousel').owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+        0:{
+            items: 1,
+            nav: false
+        },
+        600:{
+            items: 2,
+            nav: false
+        },
+        0:{
+            items: 1,
+            nav: false
+        }
+    }
+ });
+});
